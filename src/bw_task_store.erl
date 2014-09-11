3f2d920ca952b3d6984faa692c54163e717fb1f1
sanmiguel <michael.coles@gmail.com>
2014-09-11 10:52:19 +0100

    First draft of bw_task_store API

--- src/bw_task_store.erl
+++ src/bw_task_store.erl
@@ -1,3 +1,36 @@
 -module(bw_task_store).
 
+-export([create/1]).
+-export([read/1]).
+-export([update/2]).
+-export([delete/1]).
+
 -define(TASKBUCKET, <<"task_bucket">>).
+
+-type id() :: binary().
+-type task() :: tuple().
+
+-spec create(task()) ->
+    {ok, id(), task()}
+    | {error, Reason :: term()}.
+create(Task) ->
+    {error, unimplemented}.
+
+-spec read(id()) ->
+    {ok, id(), task()}
+    | {error, Reason :: term()}.
+read(ID) ->
+    {error, unimplemented}.
+
+-spec update(id(), task()) ->
+    {ok, id(), task()}
+    | {error, Reason :: term()}.
+update(ID, Task) ->
+    {error, unimplemented}.
+
+-spec delete(id()) ->
+    ok
+    | {error, Reason :: term()}.
+delete(ID) ->
+    {error, unimplemented}.
+
